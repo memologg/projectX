88ea0cd825108f35683a7fea3092c0e6bf962aaa
memologg <guardtopgamer@mail.ru>
2023-10-12 18:24:28 +0300

    calc v3
    Третья версия калькулятора

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,44 +1,50 @@
 import java.util.Scanner;
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите число: ");
-        float num1 = in.nextFloat();
-        System.out.print("Введите второе число: ");
-        float num2 = in.nextFloat();
-        System.out.print("Введите операцию (например +, -, *, /): ");
-        char operation = in.next().charAt(0);
-        System.out.print("Результат выражения: ");
-        switch (operation) {
-            case ('+'):
-                System.out.println(plus(num1, num2));
-                break;
-            case ('-'):
-                System.out.println(minus(num1,num2));
-                break;
-            case ('*'):
-                System.out.println(multiplication(num1,num2));
-                break;
-            case ('/'):
-                System.out.println(division(num1,num2));
-                break;
-            default:
-                System.out.println("Ошибка в записи выражения");
+        System.out.println("Введите выражение(возможные операции - сложение, вычитание, умножение,деление): ");
+        System.out.println("Пример выражения: 5,5 + 6,6");
+        float answer = in.nextFloat();
+        while (true) {
+            char operation = in.next().charAt(0);
+            float num2 = in.nextFloat();
+            switch (operation) {
+                case ('+'):
+                    answer = plus(answer,num2);
+                    break;
+                case ('-'):
+                    answer = minus(answer,num2);
+                    break;
+                case ('*'):
+                    answer = multiplication(answer,num2);
+                    break;
+                case ('/'):
+                    answer = division(answer,num2);
+                    break;
+                default: {
+                    System.out.println("Ошибка записи выражения");
+                    continue;
+                }
+            }
+            if (Math.round(answer) == answer) System.out.print((int) answer);
+            else System.out.print(answer);
         }
 
     }
 
-    public static float plus (float num1, float num2) {
-        return num1+num2;
-    }
-    public static float minus (float num1, float num2) {
-        return num1-num2;
-    }
-    public static float multiplication (float num1, float num2) {
-        return num1*num2;
-    }
-    public static float division (float num1, float num2) {
-        return num1/num2;
+    public static float plus(float num1, float num2) {
+        return num1 + num2;
     }
 
-}
+    public static float minus(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public static float multiplication(float num1, float num2) {
+        return num1 * num2;
+    }
+
+    public static float division(float num1, float num2) {
+            return num1 / num2;
+        }
+    }
