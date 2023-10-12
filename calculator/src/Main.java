a44e8e1b72fc73a45f397434653bea494c8269bf
memologg <guardtopgamer@mail.ru>
2023-10-12 17:14:26 +0300

    calc V1
    Первая версия калькулятора

---
+++ calculator/src/Main.java
@@ -0,0 +1,20 @@
+public class Main{
+    public static void main(String[] args) {
+        float num1 = 5, num2 = 8;
+        System.out.println(multiplication(num1,num2));
+    }
+
+    public static float plus (float num1, float num2) {
+        return num1+num2;
+    }
+    public static float minus (float num1, float num2) {
+        return num1-num2;
+    }
+    public static float multiplication (float num1, float num2) {
+        return num1*num2;
+    }
+    public static float division (float num1, float num2) {
+        return num1/num2;
+    }
+
+}
