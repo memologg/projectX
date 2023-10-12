425dbe81114b85b71b2c6e8804cd9106e4a2b53e
memologg <guardtopgamer@mail.ru>
2023-10-12 17:46:18 +0300

    calc v2
    Вторая версия калькулятора

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
-        float num1 = 5, num2 = 8;
-        System.out.println(multiplication(num1,num2));
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        float num1 = in.nextFloat();
+        System.out.print("Введите второе число: ");
+        float num2 = in.nextFloat();
+        System.out.print("Введите операцию (например +, -, *, /): ");
+        char operation = in.next().charAt(0);
+        System.out.print("Результат выражения: ");
+        switch (operation) {
+            case ('+'):
+                System.out.println(plus(num1, num2));
+                break;
+            case ('-'):
+                System.out.println(minus(num1,num2));
+                break;
+            case ('*'):
+                System.out.println(multiplication(num1,num2));
+                break;
+            case ('/'):
+                System.out.println(division(num1,num2));
+                break;
+            default:
+                System.out.println("Ошибка в записи выражения");
+        }
+
     }
 
     public static float plus (float num1, float num2) {
